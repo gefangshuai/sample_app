22329907e26b0be5f206ef5e3c00f1f9ee36238b
gefangshuai <gefangshuai@163.com>
2013-08-31 14:31:07 +0800

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.routes.draw do
   get "static_pages/home"
+
   get "static_pages/help"
+
+  get "static_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -18,7 +21,7 @@ SampleApp::Application.routes.draw do
 
   # Example resource route with options:
   #   resources :products do
-  #     member do
+  #     member do 
   #       get 'short'
   #       post 'toggle'
   #     end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  #test home page
+  describe "Home Page" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+
+    it "should have the right title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
+  end
+
+  #test help page
+  describe "Help Page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help Me')
+    end
+  end
+
+  #test about page
+
+  describe "About Page" do
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content("About Us")
+    end
+  end
+end
